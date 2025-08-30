2b0267bf0bde37e44f894e51fa63477551fcac3d
KWAshen <ashen.maliaha@gmail.com>
2025-08-30 14:38:50 +0200

    import Header, Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <h1>ciao</h1>
+  return (
+    <>
+  <><Header /><Main /></>
+  </>
+);
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+  return <div>Header Dc Comics</div>;
+};
+
+export default Header;
